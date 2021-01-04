89b75ff2ca696014427dd09c0b1548052ce5858a
Murat Mayadağ <murat-2@hotmail.com>
2021-01-04 04:28:40 +0300

    ✨feat: debounce input field

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { DebounceInput } from 'react-debounce-input';
+import styles from './SearchBar.module.css';
+import { ReactComponent as BackIcon } from '../../assets/icons/arrow-back.svg';
+
+const SearchBar = ({ searchBooksData }) => (
+  <>
+    <div className={styles.nav}>
+      <Link to="/">
+        <BackIcon className={styles.backbutton} />
+      </Link>
+      <DebounceInput
+        className={styles.searchInput}
+        minLength={2}
+        debounceTimeout={600}
+        onChange={(e) => {
+          searchBooksData(e.target.value);
+        }}
+      />
+    </div>
+  </>
+);
+SearchBar.propTypes = {
+  searchBooksData: PropTypes.instanceOf(Function),
+};
+
+SearchBar.defaultProps = {
+  searchBooksData: () => { },
+};
+
+export default SearchBar;
